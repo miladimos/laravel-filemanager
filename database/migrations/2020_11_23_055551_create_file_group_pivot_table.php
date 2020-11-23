3330acc3aa278d3d587aefc77c1bcc5e901668ef
miladimos <message.tome1331@gmail.com>
2020-11-23 09:27:45 +0330

    add file_group_pivot_table migration

---
+++ database/migrations/2020_11_23_055551_create_file_group_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFileGroupPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_group_pivot', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('file_id');
+            $table->foreignId('group_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_group_pivot');
+    }
+}
