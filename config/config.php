3cccebad238544d6ab1ceb8b8e2ccf529578245e
miladimos <message.tome1331@gmail.com>
2020-12-01 14:58:12 +0330

    s

--- config/config.php
+++ config/config.php
@@ -257,6 +257,53 @@ return [
     */
 //    'default_uploads_folder' => env('DEFAULT_UPLOADS_FOLDER', \Vmorozov\FileUploads\FilesSaver::DEFAULT_UPLOADS_FOLDER),
 
+   * Local Storage Path.
+     */
+    'rootPath' => 'images',
+
+    /*
+     * either disk or cloud
+     */
+    'filesystem' => 'disk',
+
+    /*
+     * Do you like to reduce image size?
+     */
+    'compressSize' => true,
+
+    /*
+     * Exif Data
+     */
+    'exif' => true,
+
+    /*
+     * Maximum weight of image. Leave blank or false if you do not like shrink your images
+     */
+    'maxWidth' => env('PHOTO_IMAGE_MAX_WIDTH', 800),
+
+    /*
+     * Maximum height of image. Leave blank or false if you do not like shrink your images
+     */
+    'maxHeight' => env('PHOTO_IMAGE_MAX_HEIGHT', 450),
+
+    /*
+     * How many size of your image you want.
+     */
+    'sizes' => [
+    /*
+     * Thumbnail size in pixel
+     */
+    'thumbnail' => [
+        /*
+         * Path are relative to rootPath.
+         * Suppose rootPath is photos and thumbnail path is thumbnails.
+         * Then your thumbnail full path will be photos/thumbnails
+         */
+        'path' => 'thumbnails',
+        'height' => 250,
+        'width' => 250,
+    ],
+],
 
     'files' => [
         'logo' => [
